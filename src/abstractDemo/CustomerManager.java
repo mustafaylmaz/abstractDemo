f1e9d52cf08c5287198b7751e413f41473dc2e84
mustafaylmaz <mustafaa.ylmaz@gmail.com>
2021-05-02 22:57:17 +0300

    .

---
+++ src/abstractDemo/CustomerManager.java
@@ -0,0 +1,18 @@
+package abstractDemo;
+
+public class CustomerManager {
+	
+	
+	BaseDatabaseManager databaseManager;
+	
+	
+	
+
+
+
+
+	public void getCustomers() {
+		databaseManager.getData();
+	}
+ 
+}
---
+++ src/abstractDemo/SQLDatabaseManager.java
@@ -0,0 +1,9 @@
+package abstractDemo;
+
+public class SQLDatabaseManager extends BaseDatabaseManager{
+	@Override
+	public void getData() {
+		System.out.println("SQL Verisi Listelendi");
+		
+	}
+}
